3f97cc99103317018b4cd5638bf74008618cb9a6
Vamshi Kumar <vamshigvk143@gmail.com>
2025-07-23 20:38:54 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,14 +12,11 @@ const {
 
 const { protect } = require('../middleware/authMiddleware.js');
 
-// --- Public Routes ---
 router.post('/', registerUser);
 router.post('/login', loginUser);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
-
-// --- Private/Protected Routes ---
-router.get('/profile', protect, getUserProfile); // Should be camelCase
-router.put('/profile', protect, updateUserProfile); // Should be camelCase
+router.get('/profile', protect, getUserProfile);
+router.put('/profile', protect, updateUserProfile);
 
 module.exports = router;
