15fe8c558729954ec93429b748a5470554741449
Vamshi Kumar <vamshigvk143@gmail.com>
2025-07-23 20:24:41 +0530

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { 
-  registerUser, 
-  loginUser, 
-  forgotPassword, 
+const {
+  registerUser,
+  loginUser,
+  forgotPassword,
   resetPassword,
   getUserProfile,
   updateUserProfile
