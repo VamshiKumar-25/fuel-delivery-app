470dddffe2fe94b926aaf9de9ec0de5530f00758
Vamshi Kumar <vamshigvk143@gmail.com>
2025-07-23 20:38:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,35 @@
+const path = require('path');
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const fuelTypeRoutes = require('./routes/fuelTypeRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+
+dotenv.config();
+connectDB();
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/fueltypes', fuelTypeRoutes);
+app.use('/api/orders', orderRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
+
 const PORT = process.env.PORT || 5001;
-
-app.get('/', (req, res) => {
-  res.send('--- MINIMAL SERVER TEST IS RUNNING ---');
-});
-
 app.listen(PORT, () => {
-  console.log(`Minimal server test started on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
